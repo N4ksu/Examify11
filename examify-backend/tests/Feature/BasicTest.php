e9d4d9ccdf082abe452008e80e47958a16c6a27d
N4ksu <162102010+N4ksu@users.noreply.github.com>
2026-04-10 12:13:46 -0700

    Create BasicTest.php

---
+++ examify-backend/tests/Feature/BasicTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class BasicTest extends TestCase
+{
+    public function test_basic_truth(): void
+    {
+        $this->assertTrue(true);
+    }
+}
